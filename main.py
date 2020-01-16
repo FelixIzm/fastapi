90fc1c1ffdce7c2129fdd10ab4b57a09ba27b393
FelixIzm <for_cv@mail.ru>
2020-01-16 15:39:37 +0300

    heroku 16.01.2020 15:39

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from string import Template
 from datetime import datetime
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 app = FastAPI()
 
@@ -14,6 +16,23 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{app_type}")
+async def read_item(app_type: str, unit: str = None, last_name: str = None, d_from: str = None, d_to: str = None):
+    stat = ''
+    content = ''
+    if(app_type == 'heroes'):
+        return {"app_type": app_type, 'last_name':last_name, "d_from": d_from, "d_to":d_to}
+    elif(app_type=='documents'):
+        if(unit is None):
+                stat = "Error"
+                content += 'Не заполнен параметр unit/'
+        if(d_from is None):
+                stat ="Error"
+                content += 'Не заполнен параметр d_from/'
+        if(d_to is None):
+                stat ="Error"
+                content += 'Не заполнен параметр d_to/'
+        if(stat!='Error'):
+               return {"status": 'success' ,'content': BASE_DIR}
+    else:
+        return {"action": 'not defined'}
